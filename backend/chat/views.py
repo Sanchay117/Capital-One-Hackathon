1cb8563be1911a48651ef24a239c57370f9264f7
Harsh <harsh23233@iiitd.ac.in>
2025-08-11 01:02:08 +0530

    Fixed runserver error

--- backend/chat/views.py
+++ backend/chat/views.py
@@ -5,7 +5,6 @@ from rest_framework.response import Response
 from rest_framework_simplejwt.views import TokenObtainPairView
 from .models import ChatMessage
 from .serializers import RegisterSerializer, ChatMessageSerializer
-from main import generate_answer
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view, permission_classes  # add decorator imports
 
